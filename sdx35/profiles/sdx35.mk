14c5f0ab6dba75be6ea73d45c7762772f6008649
shoudil <quic_shoudil@quicinc.com>
2023-06-29 22:44:13 +0800

    owrt/targets: add audio package groups for mbb profile

--- sdx35/profiles/sdx35.mk
+++ sdx35/profiles/sdx35.mk
@@ -31,6 +31,7 @@ define Profile/mbb
 		$(QTIWLAN) $(QTIWLANPROP) \
 		$(QTICOREINTERNAL) \
 		$(QTIPPATPROP) \
+		$(QTIAUDIO) $(QTIARGS) $(QTIPAL) $(QTIAGM) $(QTIAUDIOPROP) \
 		-edk2 -mkbootimg -linux-msm-5.4_dt -lacpd libtirpc -swconfig
 endef
 
