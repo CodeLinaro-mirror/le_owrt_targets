a5b1d8bf140f7437125efdb953e51606193af3c2
Bulbul Dabi <quic_bdabi@quicinc.com>
2023-07-06 03:07:52 +0530

    Support AB builds for sdx75
    Copy rootfs to rootfs_ab
    opkg install ABPackages_Install and
    opkg remove ABPackages_Remove from rootfs_ab
    
    Change-Id: I55dafb74fe17f9c0dab936ae098aeffffba25b3e

--- sdx75/image/generate-ubinize-cfg.mk
+++ sdx75/image/generate-ubinize-cfg.mk
@@ -33,3 +33,47 @@ define Image/Build/generate-ubinize-cfg
 		echo vol_size=\"6MiB\" >> $(UBINIZE_CFG); \
 	)
 endef
+
+define Image/Build/generate-ubinize-cfg-ab
+	(       echo \[sysfs_a_volume\] > $(UBINIZE_CFG_AB); \
+		echo mode=ubi >> $(UBINIZE_CFG_AB); \
+		echo image=\"sysfs.ubifs\" >> $(UBINIZE_CFG_AB); \
+		echo vol_id=0 >> $(UBINIZE_CFG_AB); \
+                echo vol_type=dynamic >> $(UBINIZE_CFG_AB); \
+                echo vol_name=rootfs_a >> $(UBINIZE_CFG_AB); \
+                echo vol_size=\"$(ROOTFS_VOLUME_SIZE)\" >> $(UBINIZE_CFG_AB); \
+		echo \[sysfs_b_volume\] >> $(UBINIZE_CFG_AB); \
+		echo mode=ubi >> $(UBINIZE_CFG_AB); \
+		echo image=\"sysfs.ubifs\" >> $(UBINIZE_CFG_AB); \
+		echo vol_id=1 >> $(UBINIZE_CFG_AB); \
+		echo vol_type=dynamic >> $(UBINIZE_CFG_AB); \
+		echo vol_name=rootfs_b >> $(UBINIZE_CFG_AB); \
+		echo vol_size=\"$(ROOTFS_VOLUME_SIZE)\" >> $(UBINIZE_CFG_AB); \
+                echo \[usrfs_volume\] >> $(UBINIZE_CFG_AB); \
+                echo mode=ubi >> $(UBINIZE_CFG_AB); \
+                echo image=\"userfs.ubifs\" >> $(UBINIZE_CFG_AB); \
+                echo vol_id=2 >> $(UBINIZE_CFG_AB); \
+                echo vol_type=dynamic >> $(UBINIZE_CFG_AB); \
+                echo vol_name=usrfs >> $(UBINIZE_CFG_AB); \
+                echo vol_flags=autoresize >> $(UBINIZE_CFG_AB); \
+                echo \[cache_volume\] >> $(UBINIZE_CFG_AB); \
+                echo mode=ubi >> $(UBINIZE_CFG_AB); \
+                echo vol_id=3 >> $(UBINIZE_CFG_AB); \
+                echo vol_type=dynamic >> $(UBINIZE_CFG_AB); \
+                echo vol_name=cachefs >> $(UBINIZE_CFG_AB); \
+                echo vol_size=\"15MiB\" >> $(UBINIZE_CFG_AB); \
+                echo \[systemrw_volume\] >> $(UBINIZE_CFG_AB); \
+                echo mode=ubi >> $(UBINIZE_CFG_AB); \
+                echo vol_id=4 >> $(UBINIZE_CFG_AB); \
+                echo vol_type=dynamic >> $(UBINIZE_CFG_AB); \
+                echo vol_name=systemrw >> $(UBINIZE_CFG_AB); \
+                echo vol_size=\"6MiB\" >> $(UBINIZE_CFG_AB); \
+                echo \[persist_volume\] >> $(UBINIZE_CFG_AB); \
+                echo mode=ubi >> $(UBINIZE_CFG_AB); \
+                echo vol_id=5 >> $(UBINIZE_CFG_AB); \
+                echo vol_type=dynamic >> $(UBINIZE_CFG_AB); \
+                echo vol_name=persist >> $(UBINIZE_CFG_AB); \
+                echo vol_size=\"6MiB\" >> $(UBINIZE_CFG_AB); \
+        )
+endef
+
