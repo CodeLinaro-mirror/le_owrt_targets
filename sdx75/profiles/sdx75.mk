86394d5429772b720412ef1f6a6101ae6c42fe1f
Harikrishnan Hariharan <quic_hahariha@quicinc.com>
2023-11-05 15:17:00 +0530

    location:Enable location for sdx75 profile mbb-512
    Location enablement changes for Pinancles 512 MB(MBB) DDR Variant
    
    Change-Id: I2da9cddeee820d5a6bd8c3efc7bd5c553a99380c
    CRs-Fixed: 3656485

--- sdx75/profiles/sdx75.mk
+++ sdx75/profiles/sdx75.mk
@@ -81,6 +81,7 @@ define Profile/mbb-512
                 $(COREBSP_UTILS) $(UTILS) \
                 $(QTIBSP) $(QTIBSPPROP) $(QTICORE) $(QTICOREPROP) $(QTISSMGR) $(QTISSMGRPROP) $(QTINTERNAL) \
                 $(QTISENSORSPROP) $(QTIDATA512M) $(QTIDATAPROP512M) \
+                $(QTILOCATION) $(QTILOCATIONPROP) $(QTILOCATIONINTERNAL) \
                 -edk2 -mkbootimg -linux-msm-5.4_dt -lacpd libtirpc -swconfig $(RECOVERYUPDATER)
 endef
 
