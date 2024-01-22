8550ff882dd494dee08c1a42d3a37c5688677c9c
shoudil <quic_shoudil@quicinc.com>
2024-01-22 11:11:50 +0800

    owrt/targets: add dig command support
    Enable bind and bind-dig packages in mbb profile to introduce dig
    command needed for network case.

--- sdx35/profiles/sdx35.mk
+++ sdx35/profiles/sdx35.mk
@@ -39,7 +39,7 @@ define Profile/mbb
 		$(QTIPPATPROP) \
 		$(QTIDATAINTERNAL) \
 		$(QTIAUDIO) $(QTIARGS) $(QTIPAL) $(QTIAGM) $(QTIAUDIOPROP) \
-		-edk2 -mkbootimg -linux-msm-5.4_dt -lacpd libtirpc subsystem-ramdump -swconfig $(RECOVERYUPDATER)
+		-edk2 -mkbootimg -linux-msm-5.4_dt -lacpd bind bind-dig libtirpc subsystem-ramdump -swconfig $(RECOVERYUPDATER)
 endef
 
 define Profile/mbb/Description
