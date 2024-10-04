7c3f7567dd91117ed0bda3a82289618e43a9b6bb
Vinod Kumar Reddy <quic_vreddyk@quicinc.com>
2024-10-04 17:15:36 +0530

    sdx85: DM_Verity Changes
    Change-Id: I0273209522dac424ee7a32eeb61136a0afbf48bd
    Signed-off-by: Vinod Kumar Reddy <quic_vreddyk@quicinc.com>

--- sdx85/image/getmetadata.py
+++ sdx85/image/getmetadata.py
@@ -50,7 +50,7 @@ def append_verity_metadata_to_system_image2(system_image_raw_path, system_images
             os.remove(system_images_dir +'/verity/verityHash')
             os.remove(system_images_dir +'/verity/verityFEC')
         cmd = 'veritysetup format '+ system_images_dir+'/verity/system.img.raw' +' '+ verity_hash_file +' --fec-device ' +verity_fec_file+'  --fec-roots ' + str(FEC_ROOTS)+' --salt ' +FIXED_SALT
-        proc = subprocess.Popen(cmd, shell=False, stdout=subprocess.PIPE)
+        proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
         VERITY_META = proc.communicate()[0]
         with open(system_images_dir +'/verity/verity_meta_data.txt', 'w') as f_metadata:
             entries = VERITY_META.decode('utf-8').split("\n")[1:-1]
@@ -89,7 +89,7 @@ def append_verity_metadata_to_system_image2(system_image_raw_path, system_images
 #           print( '-->'+str(count) +'.....' + str(adjustedSystemSize))
             cmd = 'fakeroot '+staging_dir_hostpkg+'/bin/make_ext4fs '+ SELINUX_EXT4_OPTS + ' -B ' +system_images_dir+'/system.map -a / -b 4096  -l '+ str(adjustedSystemSize)+' '+ system_image_raw_path +' ' + rootfs_dir 
 #           result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE )
-            result = subprocess.run(cmd, shell=False)
+            result = subprocess.run(cmd, shell=True)
 #           print('result -->'+ str(result.returncode) +result.stdout.decode('utf-8'))
 #           we see some time return code as  returncode=1, stdout=b'', stderr=b'failed to open block_list_file: Permission denied
             systemSize = os.stat(system_image_raw_path).st_size
@@ -121,7 +121,7 @@ def append_verity_metadata_to_system_image2(system_image_raw_path, system_images
 # Convert to sparse image
     img2simg = staging_dir_hostpkg + '/bin/img2simg'
     cmd =  img2simg + " %s %s " % (system_images_dir+'/verity/system.img.raw', system_images_dir + '/system.img')
-    ret = subprocess.call(cmd, shell=False)
+    ret = subprocess.call(cmd, shell=True)
     if ret != 0:
             print("-->!!!Repacking of system image to  Sparse Failed cmd:%s" % cmd)
 #Sign the hash withe know key ,
@@ -130,7 +130,7 @@ def append_verity_metadata_to_system_image2(system_image_raw_path, system_images
         hash_text.close()
 #copy the certs which keep updating with kernel build 
     cmd =  'openssl smime -sign -nocerts -noattr -binary  -in '+system_images_dir+'/verity/roothash.txt -inkey '+ system_images_dir+'/verity_key.pem  -signer '+system_images_dir+'/verity_cert.pem  -outform der -out '+ system_images_dir+'/verity/verity_sig.txt'
-    ret = subprocess.call(cmd, shell=False)
+    ret = subprocess.call(cmd, shell=True)
 
 
 
@@ -139,17 +139,17 @@ def generate_boot_images(kdir,kcmdline,kernel_baseaddr, out_images_path):
            vcmdline = c_file.read()
            c_file.close()
      print("Veritycmdline =" + vcmdline)
-     cmdline= kcmdline +'dm-mod.waitfor=/dev/dm-0'+' '+vcmdline
+     cmdline= kcmdline +' '+'dm-mod.waitfor=/dev/dm-0'+' '+vcmdline
    #  os.chdir(kdir)
    #  cmd = 'build-tools/mkbootimg/mkbootimg.py --kernel Image 	--cmdline "' +cmdline+'" --pagesize 4096 --base '+kernel_baseaddr + ' --header_version 2 --ramdisk /dev/null --ramdisk_offset 0x0 --dtb ' + 'dtb.img --output '+ out_images_path+'/boot.img'
      cmd = kdir+ '/build-tools/mkbootimg/mkbootimg.py  --kernel '+ kdir + '/Image --cmdline  "' + cmdline+ '" --pagesize 4096 --base ' + str(kernel_baseaddr)+' '+' --header_version 2 --ramdisk /dev/null --ramdisk_offset 0x0 --dtb '+ kdir + '/dtb.img --output '+ out_images_path+'/boot.img'
-     ret = subprocess.call(cmd, shell=False)
+     ret = subprocess.call(cmd, shell=True)
      if ret != 0:
           print("-->!!!!Generation of verity boot image failed .%s" % cmd)
 
    #  cmd = 'build-tools/mkbootimg/mkbootimg.py --kernel Image 	--cmdline "' +kcmdline+'" --pagesize 4096 --base '+kernel_baseaddr + ' --header_version 2 --ramdisk /dev/null --ramdisk_offset 0x0 --dtb ' + 'dtb.img --output '+ out_images_path+'/boot.noverity.img'
      cmd = kdir+ '/build-tools/mkbootimg/mkbootimg.py  --kernel '+ kdir + '/Image --cmdline  "' + kcmdline+ '" --pagesize 4096 --base ' + str(kernel_baseaddr)+' '+' --header_version 2 --ramdisk /dev/null --ramdisk_offset 0x0 --dtb '+ kdir + '/dtb.img --output '+ out_images_path+'/boot.noverity.img'
-     ret = subprocess.call(cmd, shell=False)
+     ret = subprocess.call(cmd, shell=True)
      if ret != 0:
           print("-->!!!!Generation of noverity boot image failed .%s" % cmd)
 
