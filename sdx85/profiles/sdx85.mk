bea94abd89b4199477b9f0594df7259254e45d7d
Tarun Kumar Vvs <quic_tvvs@quicinc.com>
2024-10-15 12:02:38 +0530

    Disable feeds for MBB-Min profile on Kobuk
    Change-Id: Ia77a1f891283f681b856ce81066ddf6b30970d28

--- sdx85/profiles/sdx85.mk
+++ sdx85/profiles/sdx85.mk
@@ -52,10 +52,8 @@ define Profile/mbb-min
         PACKAGES:=$(OPENWRT_STANDARD) \
                 $(COREBSP_UTILS) $(UTILS) \
                 $(QTIBSP) $(QTIBSPPROP) $(QTICORE) $(QTICOREPROP) $(QTISSMGR) $(QTISSMGRPROP) \
-                $(QTIDATA) $(QTIDATAPROP) $(QTICTAINTERNAL) $(QTISSDK) \
-                $(QTILOCATION) $(QTILOCATIONPROP) $(QTILOCATIONINTERNAL) $(QTINTERNAL) \
-                $(QTISECURITY) $(QTISECURITYPROP) $(QTISECURITYINTERNAL) $(QTIDATAINTERNAL) $(QTISENSORSPROP) $(QTIMSDCPROP) \
-                -edk2 -mkbootimg -linux-msm-5.4_dt -lacpd libtirpc -swconfig -lftp $(RECOVERYUPDATER)
+                $(QTISENSORSPROP) \
+                -edk2 -mkbootimg -linux-msm-5.4_dt -lacpd libtirpc -swconfig -lftp
 endef
 
 define Profile/mbb-min/Description
