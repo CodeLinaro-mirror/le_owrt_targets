b56c71af58c6275415a359c979bdd428e996cd15
divypate <quic_divypate@quicinc.com>
2025-04-21 22:25:45 +0530

    Revert "Added Security to mbb-min and mbb-512 profiles in sdx85.mk"
    This reverts commit d17aa7558869bb72e327fbcb298adb0a59b1511d.
    
    Change-Id: Iff6eadce4699447281e45c06bac4583b7de2688b

--- sdx85/profiles/sdx85.mk
+++ sdx85/profiles/sdx85.mk
@@ -69,8 +69,7 @@ define Profile/mbb-min
                 $(QTILOCATION) $(QTILOCATIONPROP) $(QTILOCATIONINTERNAL) \
                 $(QTIDATA) $(QTIDATAPROP) $(QTIDATAINTERNAL) \
                 $(QTISENSORSPROP) $(QTINTERNAL) \
-               	$(QTISECURITY) $(QTISECURITYPROP) $(QTISECURITYINTERNAL)\
-                $(QTITELSDK) $(QTITELSDKPROP) \
+		$(QTITELSDK) $(QTITELSDKPROP) \
                 -edk2 -mkbootimg -linux-msm-5.4_dt -lacpd libtirpc -swconfig -lftp
 endef
 
@@ -88,7 +87,6 @@ define Profile/mbb-512
                 $(QTIBSP) $(QTIBSPPROP) $(QTICORE) $(QTICOREPROP) $(QTISSMGR) $(QTISSMGRPROP) \
                 $(QTISENSORSPROP) $(QTIDATA512M) $(QTIDATAPROP512M) $(QTINTERNAL) \
                 $(QTILOCATION) $(QTILOCATIONPROP) $(QTILOCATIONINTERNAL) \
-                $(QTISECURITY) $(QTISECURITYPROP) $(QTISECURITYINTERNAL) \
                 -edk2 -mkbootimg -linux-msm-5.4_dt -lacpd libtirpc -swconfig -qdss_config \
                 -lftp -rproc-tracing -kpigen
 endef
