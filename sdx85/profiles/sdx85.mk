8606448443b2d643d3f19b6f170dfded261a3bce
K Venkata Sneha <vkuppada@qti.qualcomm.com>
2025-08-26 10:15:53 +0530

    sdx85: Add support for new profile 'cpe-v1'
    Introduced 'cpe-v1' profile for Kobuk for Unified WiFi
    driver development
    
    Change-Id: I444fe0e1b225458f86650fc82b15a432ff6f20b8

--- sdx85/profiles/sdx85.mk
+++ sdx85/profiles/sdx85.mk
@@ -107,6 +107,28 @@ endef
 
 $(eval $(call Profile,cpe-tarang))
 
+define Profile/cpe-v1
+	NAME:=Qualcomm Technologies Inc., Kobuk' CPE-V1 Profile
+	PACKAGES:=$(OPENWRT_STANDARD) \
+		$(COREBSP_UTILS) $(UTILS) \
+		$(QTIBSP) $(QTIBSPPROP) $(QTICORE) $(QTICOREPROP) $(QTISSMGR) $(QTISSMGRPROP) \
+		$(QTICOREINTERNAL) $(QTINTERNAL) \
+		$(QTILOCATION) $(QTILOCATIONPROP) $(QTILOCATIONINTERNAL) \
+		$(QTIDATA) $(QTIDATAPROP) $(QTIDATAINTERNAL) \
+		$(QTIPERFPROP) \
+		$(QTIIPQ) $(QTIIPQPROP) \
+		$(QTISECURITY) $(QTISECURITYPROP) $(QTISECURITYINTERNAL) \
+		$(QTIMSDCPROP) $(QTIEMERGENCYALERTPROP) \
+		-edk2 -mkbootimg -linux-msm-5.4_dt -lacpd libtirpc -swconfig \
+		-iw-full
+endef
+
+define Profile/cpe-v1/Description
+	CPE-V1 sdx85 package set configuration.
+	Enables complete set of modules for sdx85 target.
+endef
+
+$(eval $(call Profile,cpe-v1))
 
 define Profile/mbb-min
         NAME:=Qualcomm Technologies Inc., Kobuk' MBB Minimal Profile
